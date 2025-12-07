9d77ba532b6b5d85575ba306c3f4a942ae83c5d1
Senneseph <senneseph@gmail.com>
2025-12-07 17:40:19 -0500

    Remove tracked .tar files and honor .gitignore

--- handlers/eb6c2ae3-c105-4d3e-b726-ab170dcd2263/src/main.rs
+++ handlers/eb6c2ae3-c105-4d3e-b726-ab170dcd2263/src/main.rs
@@ -3,19 +3,19 @@ use edge_hive_sdk::prelude::*;
 
 mod handler;
 
-fn main() {{
-    loop {{
-        match edge_hive_sdk::ipc::read_request() {{
-            Ok(req) => {{
+fn main() {
+    loop {
+        match edge_hive_sdk::ipc::read_request() {
+            Ok(req) => {
                 let response = handler::handle(req);
-                if let Err(e) = edge_hive_sdk::ipc::send_response(response) {{
-                    eprintln!("Failed to send response: {{}}", e);
-                }}
-            }}
-            Err(e) => {{
-                eprintln!("Failed to read request: {{}}", e);
+                if let Err(e) = edge_hive_sdk::ipc::send_response(response) {
+                    eprintln!("Failed to send response: {}", e);
+                }
+            }
+            Err(e) => {
+                eprintln!("Failed to read request: {}", e);
                 break;
-            }}
-        }}
-    }}
-}}
+            }
+        }
+    }
+}
